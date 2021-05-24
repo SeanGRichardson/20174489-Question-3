72e2030fb9f9d1086a34428dc75fbbd3dadb5870
SeanGRichardson <80196242+SeanGRichardson@users.noreply.github.com>
2021-05-24 21:04:41 +0200

    Update

--- code/Reply_Plot.R
+++ code/Reply_Plot.R
@@ -4,7 +4,7 @@ Reply_Plot <- function(Data){
   
   Plot <- Data %>% ggplot() + geom_bar(aes(x = name, y = Total_Replies, fill= name), stat = 'identity') + 
     geom_label(aes(x = name, y = Total_Replies, label = round(Average_Replies))) +
-    labs(title= "Total and Average Replies")
+    labs(title= "Total and Average Replies") + theme(axis.text.x = element_blank())
   
   
   Plot
